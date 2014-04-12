7db3906385dc16ab4147d559ee4f4c904370b450
Fajkowsky <dawid.fajkowski@skygate.pl>
2014-04-12 20:03:06 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+requires = [
+]
+
+setup(name='HarnasAutomater',
+      version='0.1',
+      author='',
+      author_email='',
+      url='',
+      install_requires=requires,
+      packages=find_packages()
+)

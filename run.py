86c1e592986f03cd59fe561c2d5e8a4bf16562ff
Fajkowsky <dawid.fajkowski@skygate.pl>
2014-04-12 20:34:55 +0200

    added settings file and validating url

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
+import argparse
+from src import settings
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link', metavar='url', nargs=1)
+    return parser
+
+def valid_arg(args):
+    for item in settings.sites:
+        if item in args:
+            return True
+    return False
+
 
 if __name__ == '__main__':
-    pass
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if valid_arg(*args.link):
+        print("Valid url!")
+    else:
+        print("Not valid url!")
+
---
+++ src/settings.py
@@ -0,0 +1,11 @@
+sites = (
+    'http://www.logiterm.pl/',
+    'http://www.sas.busko.pl/pl/',
+    'http://enix.pl/',
+    'http://www.biawar.com.pl/',
+    'http://www.elektromet.com.pl/',
+    'http://www.vaillant.pl/',
+    'http://www.junkers.pl/pl/pl/start/',
+    'http://www.dedietrich.pl/'
+)
+

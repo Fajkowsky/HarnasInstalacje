6e1b7dfca6a32fe448a463b809a1216f56aaf762
Fajkowsky <dawid.fajkowski@skygate.pl>
2014-04-18 22:04:49 +0200

    changes to code

--- src/uploader/__init__.py
+++ src/uploader/__init__.py
@@ -10,17 +10,19 @@ def upload_data(data):
         driver.find_element_by_class_name("button").click()
 
     def add_product(driver):
-        driver.find_element_by_xpath('//*[@id="catalog"]/a').click()
-        driver.find_element_by_xpath('//*[@id="catalog"]/ul/li[2]/a').click()
+        part = '//*[@id="catalog"]/'
+        driver.find_element_by_xpath('{}a'.format(part)).click()
+        driver.find_element_by_xpath('{}ul/li[2]/a'.format(part)).click()
         driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div/a[1]').click()
 
     def fill_form(driver, data):
+        js_script = "d = document.getElementsByTagName('pre');\
+            d[1].innerHTML = '{}';".format(data['description'])
+
         driver.find_element_by_name("product_description[2][name]").send_keys(data['title'])
         driver.find_element_by_xpath('//*[@id="cke_6"]/span[3]').click()
         driver.find_element_by_xpath('//*[@id="cke_1_contents"]').click()
-        driver.execute_script("d = document.getElementsByTagName('pre');\
-            d[1].innerHTML = '{}';".format(data['description'])
-        )
+        driver.execute_script(js_script)
 
     driver = webdriver.Chrome(chrome_driver)
     driver.get(url)
